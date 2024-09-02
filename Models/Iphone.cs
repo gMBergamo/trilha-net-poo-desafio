0c6ff83964222ddc1e5a58181a786b1eea064b88
gMBergamo <giovannibergamo88@gmail.com>
2024-09-02 19:34:49 -0300

    Desafio concluido + Implementações extras!

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,26 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo (string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} na AppStore");
+            AplicativosInstalados.Add(nomeApp);
+        }
+
+        public override void LerAplicativosInstalados()
+        {
+            Console.WriteLine("Aplicativos instalados na AppStore:");
+            foreach (var app in AplicativosInstalados)
+            {
+                Console.WriteLine(app);
+            }
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new("123456789", "Nokia 3310", "123456789", 16);
+Iphone iphone = new("987654321", "iPhone 12", "987654321", 128);
+
+Console.WriteLine(nokia.Modelo);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+nokia.InstalarAplicativo("Telegram");
+nokia.InstalarAplicativo("Signal");
+nokia.LerAplicativosInstalados();
+
+Console.WriteLine("\n"+iphone.Modelo);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+iphone.InstalarAplicativo("Facebook");
+iphone.InstalarAplicativo("Twitter");
+iphone.LerAplicativosInstalados();
